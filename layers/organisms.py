88a0ca2c10aa988e9b9c29d7b5f42a853230a41f
Nathan Lowery <lowery.nathan@gmail.com>
2014-11-03 02:23:11 -0600

    OrganismLayer cleanup

--- layers/organisms.py
+++ layers/organisms.py
@@ -14,17 +14,12 @@ class OrganismsLayer(cocos.layer.Layer):
     def add_organism(self, organism):
         s = cocos.sprite.Sprite('graphics/themes/%s/organism.png' % settings.DEFAULT_THEME)
 
-        dir = 0
-        while dir == 0:
-            dir = random.randint(-1,1)
-
         s.color = organism.color
         s.position = organism.pos_x, organism.pos_y
         s.id = organism.id
 
         s.energy_label = cocos.text.Label('0',font_name='Courier',font_size=10,anchor_x='left', anchor_y='bottom', color=(255,0,0,255))
         s.energy_label.position = 15,0
-
 
         if settings.DRAW_LABELS:
             s.add(s.energy_label)
@@ -36,21 +31,29 @@ class OrganismsLayer(cocos.layer.Layer):
         self.update_position(org)
         self.update_energy_label(org)
 
+    def get_by_id(self, id):
+        for o in self.org_sprites:
+            if o.id == id:
+                return o
+        return None
 
     def update_position(self, org):
+
+        # find the direction the sprite is moving
+        # convert to degrees of rotation
+        # add 90 for angle of initial image
         vx = (org.pos_last_x) - (org.pos_x)
         vy = (org.pos_last_y*-1) - (org.pos_y*-1)
         r = (math.degrees(math.atan2(vy,vx))) + 90
 
-        for o in self.org_sprites:
-            if o.id == org.id:
-                o.position = org.pos_x, org.pos_y
-                o.do(RotateTo(r, 0.2))
+        # find the sprite -- update location and rotation
+        o = self.get_by_id(org.id)
+        o.position = org.pos_x, org.pos_y
+        o.do(RotateTo(r, 0.2))
 
     def update_energy_label(self,org):
-        for o in self.org_sprites:
-            if o.id == org.id:
-                o.energy_label.element.text = str(org.energy)
+        o = self.get_by_id(org.id)
+        o.energy_label.element.text = str(org.energy)
 
     def remove_all(self):
         for org in self.org_sprites:
@@ -58,8 +61,7 @@ class OrganismsLayer(cocos.layer.Layer):
         self.org_sprites = []
 
     def remove_sprite(self,id):
-        for org in self.org_sprites:
-            if org.id == id:
-                self.remove(org)
-                self.org_sprites.remove(org)
+        org = self.get_by_id(id)
+        self.remove(org)
+        self.org_sprites.remove(org)
 
