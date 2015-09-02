54899b8dc304544a65dea3e92de408c53142bce9
Nathan Lowery <lowery.nathan@gmail.com>
2015-09-02 18:47:12 -0500

    trying out some tests

---
+++ organism/tests.py
@@ -0,0 +1,27 @@
+from organism import Organism
+import unittest
+import settings
+
+class OrganismTests(unittest.TestCase):
+
+    def setUp(self):
+        self.org = Organism(id=1)
+
+
+    def tearDown(self):
+        pass
+
+    def test_organism_init(self):
+        self.assertGreaterEqual(self.org.pos_x, 0)
+        self.assertGreaterEqual(self.org.pos_y, 0)
+        self.assertEqual(self.org.age, 0)
+        self.assertEqual(self.org.distance_from_food, 0)
+        self.assertEqual(self.org.energy, settings.STARTING_ENERGY)
+        self.assertNotEqual(self.org.brain, None)
+
+    def test_organism_move(self):
+        pos_x = self.org.pos_x
+        pos_y = self.org.pos_y
+        self.org.move(5,5)
+        self.assertEqual(pos_x, self.org.pos_x-5)
+        self.assertEqual(pos_y, self.org.pos_y-5)
