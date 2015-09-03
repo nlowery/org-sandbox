88d86b85d0d08906aaf3f8f788186d9cc0a335a6
Nathan Lowery <lowery.nathan@gmail.com>
2015-09-03 14:56:51 -0500

    more tests

--- organism/tests.py
+++ organism/tests.py
@@ -7,10 +7,6 @@ class OrganismTests(unittest.TestCase):
     def setUp(self):
         self.org = Organism(id=1)
 
-
-    def tearDown(self):
-        pass
-
     def test_organism_init(self):
         self.assertGreaterEqual(self.org.pos_x, 0)
         self.assertGreaterEqual(self.org.pos_y, 0)
@@ -25,3 +21,8 @@ class OrganismTests(unittest.TestCase):
         self.org.move(5,5)
         self.assertEqual(pos_x, self.org.pos_x-5)
         self.assertEqual(pos_y, self.org.pos_y-5)
+
+    def test_organism_brain(self):
+        self.assertNotEqual(self.org.brain.process_input([0,0,0,0,0,0,0,0,0]), (0,0))
+        self.assertNotEqual(self.org.brain.process_input([0,1,0,1,0,1,0,1,0]), (0,0))
+        self.assertNotEqual(self.org.brain.process_input([1,1,1,1,1,1,1,1,1]), (0,0))
---
+++ world/tests.py
@@ -0,0 +1,25 @@
+from world import Map
+import unittest
+import settings
+
+class OrganismTests(unittest.TestCase):
+
+    def setUp(self):
+        self.map = Map()
+        self.map.setup_food()
+
+    def test_map_init(self):
+        self.assertEqual(self.map.FOOD_TILE, -10)
+        self.assertEqual(self.map.EMPTY_TILE, 0)
+        self.assertEqual(self.map.TILE_SIZE, 25)
+        self.assertEqual(self.map.MAX_X, settings.SCREEN_WIDTH/self.map.TILE_SIZE)
+        self.assertEqual(self.map.MAX_Y, settings.SCREEN_HEIGHT/self.map.TILE_SIZE)
+        self.assertEqual(len(self.map.food_sources), settings.NUMBER_OF_FOOD_SOURCES)
+
+    def test_map_add_food(self):
+        num_food_sources = len(self.map.food_sources)
+        self.map.new_food()
+        self.assertEqual(len(self.map.food_sources), num_food_sources+1)
+
+    def test_map_get_tile(self):
+        self.assertEqual(self.map.get_tile(0,0), self.map.tiles[0][0])
